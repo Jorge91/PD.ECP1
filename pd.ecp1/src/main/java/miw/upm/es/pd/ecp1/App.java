5df97d949ecd0d0109219af9825272a8e871258a
Jorge91 <sirjorgete@gmail.com>
2015-10-09 17:37:59 +0200

    Initial. Jorge Rábanos

---
+++ pd.ecp1/src/main/java/miw/upm/es/pd/ecp1/App.java
@@ -0,0 +1,13 @@
+package miw.upm.es.pd.ecp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
