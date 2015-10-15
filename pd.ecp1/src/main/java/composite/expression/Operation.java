e534b960b6e9b4928c1fb2b8f741f3aba4190492
Jorgelou <sirjorgete@gmail.com>
2015-10-16 00:01:48 +0200

    Operation

--- pd.ecp1/src/main/java/composite/expression/Operation.java
+++ pd.ecp1/src/main/java/composite/expression/Operation.java
@@ -1,5 +1,18 @@
 package composite.expression;
 
-public abstract class Operation {
-	 abstract int operate(int x, int y);
+public abstract class Operation extends Expresion {
+	protected Expresion firstExpression;
+	protected Expresion secondExpression;
+	
+	boolean isComposite() {
+		return true;
+	}
+
+	public Operation(Expresion firstExpression, Expresion secondExpression) {
+		super();
+		this.firstExpression = firstExpression;
+		this.secondExpression = secondExpression;
+	}
+
+
 }
