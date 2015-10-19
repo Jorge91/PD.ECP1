ee14ffcd84fc8f10987931a3f7dca75332bb547c
Jorgelou <sirjorgete@gmail.com>
2015-10-19 20:04:28 +0200

    Manager

--- pd.ecp1/src/main/java/factoryMethod/naturalNumber/NaturalNumberManager.java
+++ pd.ecp1/src/main/java/factoryMethod/naturalNumber/NaturalNumberManager.java
@@ -2,4 +2,24 @@ package factoryMethod.naturalNumber;
 
 public class NaturalNumberManager {
 
+	private NaturalNumberCreator naturalNumberCreator;
+	private NaturalNumber naturalNumber;
+	
+	public void createNaturalNumber(int value) {
+		if (this.naturalNumberCreator != null) {
+			this.naturalNumber = this.naturalNumberCreator.createNaturalNumber(value);
+		}
+	}
+	
+	public void setNaturalNumberCreator(NaturalNumberCreator naturalNumberCreator) {
+		this.naturalNumberCreator = naturalNumberCreator;
+	}
+
+	public NaturalNumber getNaturalNumber() {
+		return naturalNumber;
+	}
+	
+	
 }
+
+
